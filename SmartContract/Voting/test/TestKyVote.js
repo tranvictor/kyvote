15a4be0a1407c15132f8fd2cfc0f3e2d99c2f9b8
manhlx <manhlx.300695@gmail.com>
2018-10-15 16:36:31 +0700

    Update tests

--- SmartContract/Voting/test/TestKyVote.js
+++ SmartContract/Voting/test/TestKyVote.js
@@ -1,18 +1,252 @@
 const KyVote = artifacts.require("KyVote");
 const BigNumber = require('bignumber.js');
+const Web3 = require('web3');
 
 contract('KyVote', function(accounts) {
-  it("Admin should be the sender of create campaign", function() {
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  }
+  var numberCampaigns = 0;
+  it("Test check campaign details should be correct as used data", function() {
     var kyVote;
+    var campaignID;
     return KyVote.deployed().then(function(instance) {
       kyVote = instance;
-      return kyVote.createCampaign("New campaign", ["option 1"], ["url 1"], 999999999999, "mikele", true);
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
     }).then(function(transaction) {
-      let campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
-      console.log("Created new campaign with ID: "+ campaignID);
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: "+ campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
       return kyVote.getCampaignDetails(campaignID);
     }).then(function(details) {
-      assert.equal(details[4], accounts[0], "Admin should be the sender of create campaign");
+      assert.equal(details[2], 999999999999, "Ending time should be set to default value");
+      assert.equal(details[3], accounts[0], "Admin should be the sender of create campaign");
+      assert.equal(details[4], true, "The campaign should allow multiple choices");
+      return kyVote.getOptionsCount(parseInt(BigNumber(details[0])));
+    }).then(function(data) {
+      assert.equal(parseInt(BigNumber(data)), 2, "The number of options should be 2");
+      return kyVote.isCampaignEnded(campaignID);
+    }).then(function(isEnded) {
+      assert.equal(isEnded, false, "The new campaign should not be ended");
+      return kyVote.getCampaignWhitelistedAddresses(campaignID);
+    }).then(function(addresses) {
+      assert.equal(addresses.length, 1, "Should contain only 1 whitelisted address");
+      assert.equal(addresses[0], accounts[0], "Whitelisted address should be account 0");
+      return kyVote.getListActiveCampaignIDs();
+    }).then(function(activeIDs) {
+      assert.equal(activeIDs.length, 1, "Should have only 1 active campaigns");
+      assert.equal(activeIDs[0], campaignID, "Campaign ID should be in the list active IDs");
+    });
+  });
+  it("Test override list whitelisted addresses", function() {
+    var kyVote;
+    var campaginID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: " + campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, true, "The account 0 should be in the whitelisted addresses");
+      kyVote.updateNewWhitelistedAddresses(campaignID, []);
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      //console.log("Override whitelisted addresses with empty data");
+      assert.equal(whitelisted, false, "The account 0 should not be in the whitelisted addresses");
+      kyVote.updateNewWhitelistedAddresses(campaignID, [0x2262D4F6312805851E3B27C40db2c7282E6e4a49]);
+      //console.log("Add new whitelisted addresses: [0x2262D4F6312805851E3B27C40db2c7282E6e4a49]");
+      return kyVote.getCampaignWhitelistedAddresses(campaignID);
+    }).then(function(addrsses) {
+      assert.equal(addrsses.length, 1, "Whitelisted addresses should have 1 element");
+      assert.equal(addrsses[0], 0x2262D4F6312805851E3B27C40db2c7282E6e4a49, "0x2262D4F6312805851E3B27C40db2c7282E6e4a49 should be in the whitelisted addresses")
+    });
+  });
+  it("Test remove whitelisted addresses", function() {
+    var kyVote;
+    var campaginID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: " + campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, true, "The account 0 should be in the whitelisted addresses");
+      kyVote.removeWhitelistedAddresses(campaignID, [0x2262D4F6312805851E3B27C40db2c7282E6e4a49]);
+      // remove non whitelisted address
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, true, "The account 0 should be in the whitelisted addresses");
+      kyVote.removeWhitelistedAddresses(campaignID, [accounts[0]]);
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      //console.log("Removed account 0 from whitelisted addresses");
+      assert.equal(whitelisted, false, "The account 0 should not be in the whitelisted addresses");
+      return kyVote.getCampaignWhitelistedAddresses(campaignID);
+    }).then(function(addresses) {
+      assert.equal(addresses.length, 0, "Whitelisted address should be empty");
+    });
+  });
+  it("Test add new whitelisted addresses", function() {
+    var kyVote;
+    var campaginID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: " + campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, true, "The account 0 should be in the whitelisted addresses");
+      return kyVote.checkWhitelisted(campaignID, 0x2262D4F6312805851E3B27C40db2c7282E6e4a49);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, false, "0x2262D4F6312805851E3B27C40db2c7282E6e4a49 should be in the whitelisted addresses");
+      kyVote.addWhitelistedAddresses(campaignID, [0x2262D4F6312805851E3B27C40db2c7282E6e4a49]);
+      //console.log("Added 0x2262D4F6312805851E3B27C40db2c7282E6e4a49 to whitelisted addresses");
+      return kyVote.checkWhitelisted(campaignID, 0x2262D4F6312805851E3B27C40db2c7282E6e4a49);
+    }).then(function(whitelisted) {
+      assert.equal(whitelisted, true, "0x2262D4F6312805851E3B27C40db2c7282E6e4a49 should be in the whitelisted addresses");
+      return kyVote.getCampaignWhitelistedAddresses(campaignID);
+    }).then(function(addresses) {
+      assert.equal(addresses.length, 2, "Whitelisted addresses should have 2 elements")
+      assert.equal(addresses[0], accounts[0], "Accounts[0] should be in the whitelisted addresses");
+      assert.equal(addresses[1], 0x2262D4F6312805851E3B27C40db2c7282E6e4a49, "0x2262D4F6312805851E3B27C40db2c7282E6e4a49 should be in the whitelisted addresses");
+    });
+  });
+  it("Test stop an active (new) campaign", function() {
+    var kyVote;
+    var campaginID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: " + campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      return kyVote.isCampaignEnded(campaignID);
+    }).then(function(isEnded) {
+      assert.equal(isEnded, false, "The campagin should not be ended");
+      return kyVote.stopCampaign(campaignID);
+    }).then(function() {
+      //console.log("Stopped the campaign with ID: " + campaignID);
+      return kyVote.isCampaignEnded(campaignID);
+    }).then(function(isEnded) {
+      assert.equal(isEnded, true, "The campagin should be ended");
+    });
+  });
+  it("Test option details should be all correct", function() {
+    var kyVote;
+    var campaignID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      //console.log("Created new campaign with ID: "+ campaignID);
+      return kyVote.getListOptions(campaignID);
+      //console.log("Created new campaign with ID: "+ campaignID);
+    }).then(function(details) {
+      assert.equal(details.length, 3, "Should return 3 arrays");
+      assert.equal(details[0].length, 2, "Should have 2 options");
+      assert.equal(details[1].length, 2, "Should have 2 options");
+      assert.equal(details[2].length, 2, "Should have 2 options");
+      let id0 = parseInt(BigNumber(details[0][0]));
+      let id1 = parseInt(BigNumber(details[0][1]));
+      assert.equal(id0, 0, "First option should have id 0");
+      assert.equal(id1, 1, "Second option should have id 1");
+      return kyVote.getOption(campaignID, 0);
+    }).then(function(option0Details) {
+      assert.equal(option0Details.length, 4, "Should return 4 data for option 0");
+      assert.equal(0, parseInt(BigNumber(option0Details[0])), "First element should be id with value 0");
+      assert.equal(option0Details[3].length, 0, "Should have no voters for option 0");
+      return kyVote.getOption(campaignID, 1);
+    }).then(function(option1Details) {
+      assert.equal(option1Details.length, 4, "Should return 4 data for option 1");
+      assert.equal(1, parseInt(BigNumber(option1Details[0])), "First element should be id with value 1");
+      assert.equal(option1Details[3].length, 0, "Should have no voters for option 1");
+      return kyVote.checkWhitelisted(campaignID, accounts[0]);
+    }).then(function(isWhitelisted) {
+      assert.equal(isWhitelisted, true, "Account 0 should be whitelisted for this campaign");
+      return kyVote.getVoters(campaignID, 0);
+    }).then(function(voters0) {
+      assert.equal(voters0.length, 0, "Should have no voters for option 0");
+      return kyVote.getVoters(campaignID, 1);
+    }).then(function(voters1) {
+      assert.equal(voters1.length, 0, "Should have no voters for option 1");
+    });
+  });
+  it("Test whitelisted account should be able to vote and unvote for all options", function() {
+    var kyVote;
+    var campaignID;
+    return KyVote.deployed().then(function(instance) {
+      kyVote = instance;
+      return kyVote.createCampaign("New campaign", ["option 1", "option 2"], ["url 1", "url 2"], 999999999999, true, [accounts[0]]);
+    }).then(function(transaction) {
+      numberCampaigns++;
+      //console.log(transaction.logs[0].args);
+      campaignID = parseInt(BigNumber(transaction.logs[0].args.campaignID));
+      //console.log("Created new campaign with ID: "+ campaignID);
+      assert.equal(campaignID, numberCampaigns - 1, "Invalid campaign ID");
+      //console.log("Voting option 0");
+      kyVote.vote(campaignID, [0], {from: accounts[0]});
+      return kyVote.getVoters(campaignID, 0);
+    }).then(function(voters0) {
+      //console.log("Voted option 0");
+      assert.equal(voters0.length, 1, "Should have 1 voter for option 0");
+      assert.equal(voters0[0], accounts[0], "Voter of option 0 should be account 0");
+      //console.log("Unvoting option 0");
+      kyVote.vote(campaignID, [], {from: accounts[0]});
+      return kyVote.getVoters(campaignID, 0);
+    }).then(function(voters0) {
+      //console.log("Unvoted option 0");
+      assert.equal(voters0.length, 0, "Should have no voter for option 0");
+      return kyVote.getVoters(campaignID, 1);
+    }).then(function(voters1) {
+      assert.equal(voters1.length, 0, "Should have 0 voters for option 1");
+      //console.log("Voting option 1");
+      kyVote.vote(campaignID, [1], {from: accounts[0]});
+      return kyVote.getVoters(campaignID, 1);
+    }).then(function(voters1) {
+      //console.log("Voted option 1");
+      assert.equal(voters1.length, 1, "Should have 1 voter for option 1");
+      //console.log("Unvoting option 1");
+      kyVote.vote(campaignID, [], {from: accounts[0]});
+      return kyVote.getVoters(campaignID, 1);
+    }).then(function(voters1) {
+      //console.log("Unvoted option 1");
+      assert.equal(voters1.length, 0, "Should have 0 voters for option 1");
+      //console.log("Voting multiple options");
+      kyVote.vote(campaignID, [0, 1], {from: accounts[0]});
+      return kyVote.getVoters(campaignID, 0);
+    }).then(function(voters0) {
+      //console.log("Voted option 0");
+      assert.equal(voters0.length, 1, "Should have 1 voter for option 0");
+      return kyVote.getVoters(campaignID, 1);
+    }).then(function(voters1) {
+      //console.log("Voted option 1");
+      assert.equal(voters1.length, 1, "Should have 1 voter for option 1");
     });
   });
 })
--- SmartContract/Voting/test/TestKyVote.sol
+++ SmartContract/Voting/test/TestKyVote.sol
@@ -17,27 +17,26 @@ contract TestKyVote {
     bytes32[] memory names = new bytes32[](2);
     names[0] = bytes32("option 1");
     names[1] = bytes32("option 2");
+    // whitelisted addresses
+    address[] memory whitelistedAddresses = new address[](2);
+    whitelistedAddresses[0] = 0x2262D4F6312805851E3B27C40db2c7282E6e4a49;
+    whitelistedAddresses[1] = kyVote.owner();
     // endTime is set to be very large: 999999999999
-    kyVote.createCampaign(bytes32("New campaign"), names, names, 999999999999, bytes32("mikele"), true);
+    kyVote.createCampaign(bytes32("New campaign"), names, names, 999999999999, true, whitelistedAddresses);
 
     Assert.equal(kyVote.isCampaignEnded(numberCampaigns), false, "Campaign should be active");
     // Test number of options
     Assert.equal(kyVote.getOptionsCount(numberCampaigns), 2, "Number options is not correct");
+    // Test whitelisted
+    Assert.equal(kyVote.checkWhitelisted(numberCampaigns, 0x2262D4F6312805851E3B27C40db2c7282E6e4a49), true, "Address should be whitelisted");
+    Assert.equal(kyVote.checkWhitelisted(numberCampaigns, 0x2262d4f6312805851E3b27C40Db2C7282E6E4A48), false, "Address should not be whitelisted");
     // Test campaign details
-    (uint cID, bytes32 cName, uint cEndTime, bytes32 cCreatorID, address cAdmin, bool cMultipleChoice) = kyVote.getCampaignDetails(numberCampaigns);
+    (uint cID, bytes32 cName, uint cEndTime, address cAdmin, bool cMultipleChoice) = kyVote.getCampaignDetails(numberCampaigns);
     Assert.equal(numberCampaigns, cID, "Wrongly getting campaign");
     Assert.equal(cName, bytes32("New campaign"), "Name of campaign is not equal");
     Assert.equal(cEndTime, 999999999999, "End time of campaign is not equal");
-    Assert.equal(cCreatorID, bytes32("mikele"), "Creator ID is not equal");
     Assert.equal(cMultipleChoice, true, "Campaign should be a multiple choice");
     Assert.equal(kyVote.getTotalNumberCampaigns(), numberCampaigns + 1, "Number campaign should be increased by one after created a new campaign");
-
-    // Test option 0 data
-    (uint oID, bytes32 oName, bytes32 oUrl, bytes32[] memory oVoterIDs) = kyVote.getOption(numberCampaigns, 0);
-    Assert.equal(oID, 0, "Wrong getting option");
-    Assert.equal(oName, names[0], "Name of option is not correct set");
-    Assert.equal(oUrl, names[0], "URL of option is not correct set");
-    Assert.equal(oVoterIDs.length, 0, "New campaign all options should have 0 voters");
   }
 
   // Test create new campaign becomes running
@@ -72,101 +71,98 @@ contract TestKyVote {
   }
 
   // Test create a campaign with 2 options and only vote option 0
-  function testVoteOneOption() public {
-    KyVote kyVote = KyVote(DeployedAddresses.KyVote());
-    testCreateNewCampaign();
-    uint numberCampaigns = kyVote.getTotalNumberCampaigns();
-    // id of new campaign: numberCampaigns - 1;
-    bytes32[] memory oldVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory oldVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    uint[] memory voteOptions = new uint[](1);
-    voteOptions[0] = 0;
-    kyVote.vote(bytes32("mikele"), numberCampaigns - 1, voteOptions);
-    bytes32[] memory newVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory newVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    Assert.equal(oldVoterIDs0.length + 1, newVoterIDs0.length, "New voter should be added to option 0");
-    Assert.equal(oldVoterIDs1.length, newVoterIDs1.length, "New voter should not be added to option 1");
-    uint i;
-    bool contains = false;
-    for(i = 0; i < newVoterIDs0.length; i++) {
-      if (newVoterIDs0[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, true, "Voters of option 0 should contain mikele");
-    contains = false;
-    for(i = 0; i < newVoterIDs1.length; i++) {
-      if (newVoterIDs1[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, false, "Voters of option 1 should not contain mikele");
-  }
+  // function testVoteOneOption() public {
+  //   KyVote kyVote = KyVote(DeployedAddresses.KyVote());
+  //   testCreateNewCampaign();
+  //   uint numberCampaigns = kyVote.getTotalNumberCampaigns();
+  //   // id of new campaign: numberCampaigns - 1;
+  //   address[] memory oldVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory oldVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   uint[] memory voteOptions = new uint[](1);
+  //   voteOptions[0] = 0;
+  //   kyVote.vote(numberCampaigns - 1, voteOptions);
+  //   address[] memory newVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory newVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   Assert.equal(oldVoters0.length + 1, newVoters0.length, "New voter should be added to option 0");
+  //   Assert.equal(oldVoters1.length, newVoters1.length, "New voter should not be added to option 1");
+  //   uint i;
+  //   bool contains = false;
+  //   Assert.equal(contains, true, "Voters of option 0 should contain new address");
+  //   contains = false;
+  //   for(i = 0; i < newVoters1.length; i++) {
+  //     if (newVoters1[i] == kyVote.owner()) { contains = true; break; }
+  //   }
+  //   Assert.equal(contains, false, "Voters of option 1 should not contain new address");
+  // }
 
   // Test create a multiple choice campaign with 2 options and vote all options
-  function testVoteAllOptions() public {
-    KyVote kyVote = KyVote(DeployedAddresses.KyVote());
-    testCreateNewCampaign();
-    uint numberCampaigns = kyVote.getTotalNumberCampaigns();
-    // id of new campaign: numberCampaigns - 1;
-    // Get option 0 of new campaign
-    bytes32[] memory oldVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory oldVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    uint[] memory voteOptions = new uint[](2);
-    voteOptions[0] = 0;
-    voteOptions[1] = 1;
-    kyVote.vote(bytes32("mikele"), numberCampaigns - 1, voteOptions);
-    bytes32[] memory newVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory newVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    Assert.equal(oldVoterIDs0.length + 1, newVoterIDs0.length, "New voter should be added to option 0");
-    Assert.equal(oldVoterIDs1.length + 1, newVoterIDs1.length, "New voter should be added to option 1");
-    uint i;
-    bool contains = false;
-    for(i = 0; i < newVoterIDs0.length; i++) {
-      if (newVoterIDs0[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, true, "Voters of option 0 should contain mikele");
-    contains = false;
-    for(i = 0; i < newVoterIDs1.length; i++) {
-      if (newVoterIDs1[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, true, "Voters of option 1 should contain mikele");
-  }
+  // function testVoteAllOptions() public {
+  //   KyVote kyVote = KyVote(DeployedAddresses.KyVote());
+  //   testCreateNewCampaign();
+  //   uint numberCampaigns = kyVote.getTotalNumberCampaigns();
+  //   // id of new campaign: numberCampaigns - 1;
+  //   // Get option 0 of new campaign
+  //   address[] memory oldVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory oldVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   uint[] memory voteOptions = new uint[](2);
+  //   voteOptions[0] = 0;
+  //   voteOptions[1] = 1;
+  //   kyVote.vote(numberCampaigns - 1, voteOptions);
+  //   address[] memory newVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory newVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   Assert.equal(oldVoters0.length + 1, newVoters0.length, "New voter should be added to option 0");
+  //   Assert.equal(oldVoters1.length + 1, newVoters1.length, "New voter should be added to option 1");
+  //   uint i;
+  //   bool contains = false;
+  //   for(i = 0; i < newVoters0.length; i++) {
+  //     if (newVoters0[i] == kyVote.owner()) { contains = true; break; }
+  //   }
+  //   Assert.equal(contains, true, "Voters of option 0 should contain new address");
+  //   contains = false;
+  //   for(i = 0; i < newVoters1.length; i++) {
+  //     if (newVoters1[i] == kyVote.owner()) { contains = true; break; }
+  //   }
+  //   Assert.equal(contains, true, "Voters of option 1 should contain new address");
+  // }
 
   // Test create a multiple choice campaign with 2 options and vote all options
   // Then unvote only option 0
-  function testUnVoteAnOptionInMultipleChoice() public {
-    KyVote kyVote = KyVote(DeployedAddresses.KyVote());
-    testVoteAllOptions();
-    uint numberCampaigns = kyVote.getTotalNumberCampaigns();
-    // id of new campaign: numberCampaigns - 1;
-    bytes32[] memory oldVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory oldVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    uint[] memory voteOptions = new uint[](1);
-    voteOptions[0] = 1;
-    // vote option contains only 1, mean unvote option 0
-    kyVote.vote(bytes32("mikele"), numberCampaigns - 1, voteOptions);
-    bytes32[] memory newVoterIDs0 = kyVote.getVoterIDs(numberCampaigns - 1, 0);
-    bytes32[] memory newVoterIDs1 = kyVote.getVoterIDs(numberCampaigns - 1, 1);
-    Assert.equal(oldVoterIDs0.length - 1, newVoterIDs0.length, "The voter should be removed from option 0");
-    Assert.equal(oldVoterIDs1.length, newVoterIDs1.length, "The voter should be stayed in option 1");
-    uint i;
-    bool contains = false;
-    for(i = 0; i < newVoterIDs0.length; i++) {
-      if (newVoterIDs0[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, false, "Voters of option 0 should not contain mikele");
-    contains = false;
-    for(i = 0; i < newVoterIDs1.length; i++) {
-      if (newVoterIDs1[i] == bytes32("mikele")) { contains = true; break; }
-    }
-    Assert.equal(contains, true, "Voters of option 1 should contain mikele");
-  }
+  // function testUnVoteAnOptionInMultipleChoice() public {
+  //   KyVote kyVote = KyVote(DeployedAddresses.KyVote());
+  //   testVoteAllOptions();
+  //   uint numberCampaigns = kyVote.getTotalNumberCampaigns();
+  //   // id of new campaign: numberCampaigns - 1;
+  //   address[] memory oldVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory oldVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   uint[] memory voteOptions = new uint[](1);
+  //   voteOptions[0] = 1;
+  //   // vote option contains only 1, mean unvote option 0
+  //   kyVote.vote(numberCampaigns - 1, voteOptions);
+  //   address[] memory newVoters0 = kyVote.getVoters(numberCampaigns - 1, 0);
+  //   address[] memory newVoters1 = kyVote.getVoters(numberCampaigns - 1, 1);
+  //   Assert.equal(oldVoters0.length - 1, newVoters0.length, "The voter should be removed from option 0");
+  //   Assert.equal(oldVoters1.length, newVoters1.length, "The voter should be stayed in option 1");
+  //   uint i;
+  //   bool contains = false;
+  //   for(i = 0; i < newVoters0.length; i++) {
+  //     if (newVoters0[i] == kyVote.owner()) { contains = true; break; }
+  //   }
+  //   Assert.equal(contains, false, "Voters of option 0 should not contain new address");
+  //   contains = false;
+  //   for(i = 0; i < newVoters1.length; i++) {
+  //     if (newVoters1[i] == kyVote.owner()) { contains = true; break; }
+  //   }
+  //   Assert.equal(contains, true, "Voters of option 1 should contain new address");
+  // }
 
   // Test stop an active campaign
-  function testStopAnActiveCampaign() public {
-    KyVote kyVote = KyVote(DeployedAddresses.KyVote());
-    testCreateNewCampaign();
-    uint numberCampaigns = kyVote.getTotalNumberCampaigns();
-    // new campaignID is numberCampaigns - 1
-    Assert.equal(kyVote.isCampaignEnded(numberCampaigns - 1), false, "Campaign should be still active");
-    kyVote.stopCampaign(numberCampaigns - 1);
-    Assert.equal(kyVote.isCampaignEnded(numberCampaigns - 1), true, "Campaign should be ended");
-  }
+  // function testStopAnActiveCampaign() public {
+  //   KyVote kyVote = KyVote(DeployedAddresses.KyVote());
+  //   testCreateNewCampaign();
+  //   uint numberCampaigns = kyVote.getTotalNumberCampaigns();
+  //   // new campaignID is numberCampaigns - 1
+  //   Assert.equal(kyVote.isCampaignEnded(numberCampaigns - 1), false, "Campaign should be still active");
+  //   kyVote.stopCampaign(numberCampaigns - 1);
+  //   Assert.equal(kyVote.isCampaignEnded(numberCampaigns - 1), true, "Campaign should be ended");
+  // }
 }
